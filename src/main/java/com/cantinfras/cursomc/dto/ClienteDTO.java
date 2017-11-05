135437765a3eed1274e41809d907c3301e1da69d
Cantinfras <cantinfras2@gmail.com>
2017-11-05 10:57:34 -0200

    Validacao customizada: email nao repetido na atualizacao do cliente

--- src/main/java/com/cantinfras/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/cantinfras/cursomc/dto/ClienteDTO.java
@@ -7,7 +7,9 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import com.cantinfras.cursomc.domain.Cliente;
+import com.cantinfras.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
