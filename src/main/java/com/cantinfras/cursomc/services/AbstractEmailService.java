d0b2f5f20eb59337099b92f0f885f8e1cc7a74a3
Cantinfras <cantinfras2@gmail.com>
2017-11-22 22:48:24 -0200

    Esqueci a senha

--- src/main/java/com/cantinfras/cursomc/services/AbstractEmailService.java
+++ src/main/java/com/cantinfras/cursomc/services/AbstractEmailService.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 
+import com.cantinfras.cursomc.domain.Cliente;
 import com.cantinfras.cursomc.domain.Pedido;
 
 public abstract class AbstractEmailService implements EmailService {
@@ -14,11 +15,11 @@ public abstract class AbstractEmailService implements EmailService {
 	
 	@Override
 	public void sendOrderConfirmationEmail(Pedido obj) {
-		SimpleMailMessage sm = prepareSimpleMailMessageFromPedido(obj);		
+		SimpleMailMessage sm = prepareSimpleMailMessageFromPedido(obj);
 		sendEmail(sm);
 	}
 
-	private SimpleMailMessage prepareSimpleMailMessageFromPedido(Pedido obj) {
+	protected SimpleMailMessage prepareSimpleMailMessageFromPedido(Pedido obj) {
 		SimpleMailMessage sm = new SimpleMailMessage();
 		sm.setTo(obj.getCliente().getEmail());
 		sm.setFrom(sender);
@@ -27,5 +28,20 @@ public abstract class AbstractEmailService implements EmailService {
 		sm.setText(obj.toString());
 		return sm;
 	}
+	
+	@Override
+	public void sendNewPasswordEmail(Cliente cliente, String newPass) {
+		SimpleMailMessage sm = prepareNewPasswordEmail(cliente, newPass);
+		sendEmail(sm);
+	}
 
+	protected SimpleMailMessage prepareNewPasswordEmail(Cliente cliente, String newPass) {
+		SimpleMailMessage sm = new SimpleMailMessage();
+		sm.setTo(cliente.getEmail());
+		sm.setFrom(sender);
+		sm.setSubject("Solicitação de nova senha");
+		sm.setSentDate(new Date(System.currentTimeMillis()));
+		sm.setText("Nova senha: " + newPass);
+		return sm;
+	}
 }
--- src/main/java/com/cantinfras/cursomc/services/EmailService.java
+++ src/main/java/com/cantinfras/cursomc/services/EmailService.java
@@ -2,12 +2,14 @@ package com.cantinfras.cursomc.services;
 
 import org.springframework.mail.SimpleMailMessage;
 
+import com.cantinfras.cursomc.domain.Cliente;
 import com.cantinfras.cursomc.domain.Pedido;
 
 public interface EmailService {
-	
+
 	void sendOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
-
+	
+	void sendNewPasswordEmail(Cliente cliente, String newPass);
 }
